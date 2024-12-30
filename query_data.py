711e518cc7c24c884caa4a6ffdd6d369234078a8
yourstrulyMayank <harlalkamayank1996@gmail.com>
2024-12-30 23:45:03 +0530

    Fifteenth Checkpoint

--- query_data.py
+++ query_data.py
@@ -77,9 +77,7 @@ def query_rag_latest(query_text: str, db, model, latest_file):
     Instructions:
     - If the answer is present in the context, provide it clearly and concisely.
     - If the answer is not found in the context, respond exactly with "Not Specified In The Document".
-    - If the question asks for the company name, try to infer it somehow.
-    - Treat tabular data carefully and preserve its structure.
-    - If the answer is found within a table, specify the relevant row(s) and column(s).
+    - If the question asks for the company name, try to infer it somehow. Provide only the company name and no other words. If you can't infer, then say "Not Specified In The Document"
     
     """
     # Prepare the DB.
